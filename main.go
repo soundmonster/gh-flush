9b3d1ae1f02ca2700cceade083ec8e25c0e86dd7
Leo B <404293+soundmonster@users.noreply.github.com>
2025-02-11 16:46:11 +0100

    Fallback to simple output if redirected

--- main.go
+++ main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"os"
+
 	"github.com/soundmonster/gh-flush/internal/client"
 	"github.com/soundmonster/gh-flush/internal/ui"
 )
 
 func main() {
 	client := client.NewClient()
-	ui.Run(client)
-	// TODO handle character device output
-	// if this is not a TTY:
-	// client.FetchNotifications()
-	// client.ProcessNotifications()
-	// client.PrintResults()
-	// fmt.Println("Done 🎉")
+	if isTerminal() {
+		ui.Run(client)
+	} else {
+		client.FetchNotifications()
+		client.ProcessNotifications()
+		client.PrintResults()
+	}
+}
+
+func isTerminal() bool {
+	fi, err := os.Stdout.Stat()
+	if err != nil {
+		panic(err)
+	}
+	return (fi.Mode() & os.ModeCharDevice) == os.ModeCharDevice
 }
